d83e85313d742d8133922951f9436d764dc93430
Torikul Islam Rifat <17103050@iubat.edu>
2021-06-13 23:58:05 +0600

    h2 added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,10 @@
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+     
     <>
-    <h1> Welcome</h1>
+    <h1> Hello React</h1>
+    <h2>Hello world</h2>
     </>
   );
 }
